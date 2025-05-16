868f0a169f7aaad8e5e5daa90aa29b04d1a12cff
Pillosnatchaz <bagusrahadyan1@gmail.com>
2025-05-16 10:53:12 +0700

    style: web.php formatting

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ItemsController;
 use App\Http\Controllers\WarehousesController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware([
@@ -19,11 +20,36 @@ Route::middleware([
     })->name('dashboard');
 });
 
+// Displaying
+Route::get('/users', [UsersController::class, 'index'])
+    ->middleware('auth')
+    ->name('users.index');
 
-Route::get('/users', [UsersController::class, 'index'])->middleware('auth')->name('users.index');
-Route::get('/items', [ItemsController::class, 'index'])->middleware('auth')->name('items.index');
-Route::get('/warehouses', [WarehousesController::class, 'index'])->middleware('auth')->name('warehouse.index');
+Route::get('/items', [ItemsController::class, 'index'])
+    ->middleware('auth')
+    ->name('items.index');
 
-Route::post('/users', [UsersController::class, 'store'])->middleware('auth')->name('users.store');
-Route::post('/items', [ItemsController::class, 'store'])->middleware('auth')->name('items.store');
-Route::post('/warehouses', [WarehousesController::class, 'store'])->middleware('auth')->name('warehouse.store');
+Route::get('/warehouses', [WarehousesController::class, 'index'])
+    ->middleware('auth')
+    ->name('warehouse.index');
+
+Route::get('/projects', [ProjectController::class, 'index'])
+    ->middleware('auth')
+    ->name('project.index');
+
+// Storing data
+Route::post('/users', [UsersController::class, 'store'])
+    ->middleware('auth')
+    ->name('users.store');
+
+Route::post('/items', [ItemsController::class, 'store'])
+    ->middleware('auth')
+    ->name('items.store');
+
+Route::post('/warehouses', [WarehousesController::class, 'store'])
+    ->middleware('auth')
+    ->name('warehouse.store');
+
+Route::post('/projects', [ProjectController::class, 'index'])
+    ->middleware('auth')
+    ->name('project.store');
